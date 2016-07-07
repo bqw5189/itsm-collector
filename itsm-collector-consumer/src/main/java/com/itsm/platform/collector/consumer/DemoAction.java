6f4af7fbe33c6862620618e02cf8a017f0e6b657
tom <tom@daoshenggroup.com>
2016-07-07 15:11:45 +0800

    init

--- itsm-collector-consumer/src/main/java/com/itsm/platform/collector/consumer/DemoAction.java
+++ itsm-collector-consumer/src/main/java/com/itsm/platform/collector/consumer/DemoAction.java
@@ -14,9 +14,6 @@
  * limitations under the License.
  */
 package com.itsm.platform.collector.consumer;
-import com.itsm.platform.collector.entity.Account;
-import com.itsm.platform.collector.entity.From;
-import com.itsm.platform.collector.facade.SignInVO;
 
 
 /**
@@ -24,35 +21,9 @@ import com.itsm.platform.collector.facade.SignInVO;
  */
 public class DemoAction {
 
-    private AccountService accountService;
-
-    public AccountService getAccountService() {
-        return accountService;
-    }
-
-    public void setAccountService(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     public void start() throws Exception {
 
-
-        Account account = new Account();
-        account.setName("白群伟");
-        account.setPassword("123456");
-
-        From from = new From();
-        from.setAgentId("test");
-        from.setAgentName("R03025");
-        from.setAgentType("MacOS");
-        from.setClientVersion("1.0");
-        from.setLocation("1,2,3");
-        from.setOsVersion("10.10.5");
-
-        account.setFrom(from);
-
-
-        System.out.println("SUCESS: signin user with: " + accountService.signIn(account));
     }
 
 }
